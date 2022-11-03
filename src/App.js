119b638e28e0e59964f127a84a7e0eb6e43b9606
MishaKravchenko <mishakravchenko.1998@gmail.com>
2022-11-03 19:06:08 +0200

    @!I#Y!@$OIUU@!O$IU!@OI$P!@#$!*@)(

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import './App.css';
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 import {Countries} from "./components/Countries/Countries";
-import {Pagination} from "./components/Pagination/Pagination";
-import {Form} from "./components/Form/Form";
 
 // https://www.youtube.com/watch?v=s59kRbD4Sw8&list=PLkUJHNMBzmtRuSUgA_9g0lmMC_Y1TW2K1&index=12
 function App() {
 
     const [countries, setCountries] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [countriesPerPage] = useState(48);
+    const [value, setValue] = useState('');
 
-    const paginate = (pageNumber) => setCurrentPage(pageNumber);
-    const nextPage = () => setCurrentPage(prevState => prevState + 1)
-    const prevPage = () => setCurrentPage(prevState => prevState - 1)
+    const filteredCountries = countries.filter(country => {
+            return country.name.official.toLowerCase().includes(value.toLowerCase())
+        }
+    )
 
 
     useEffect(() => {
@@ -22,20 +20,14 @@ function App() {
             .then(res => setCountries(res.data))
     }, [])
 
-    const lastCountryIndex = currentPage * countriesPerPage;
-    const firstCountryIndex = lastCountryIndex - countriesPerPage;
-    const currentCountry = countries.slice(firstCountryIndex, lastCountryIndex)
-
     return (
         <div className={'wrapper'}>
-            <Form/>
+            <form>
+                <input className="input" type="text" placeholder="Type to Search..."
+                       onChange={(e) => setValue(e.target.value)}/>
+            </form>
             <div className="container">
-                {currentCountry.map(country => <Countries key={country.name.common} country={country}/>)}
-            </div>
-            <div className={"paginate"}>
-                <button className={"btn"} onClick={prevPage}>PrevPage</button>
-                <Pagination countriesPerPage={countriesPerPage} totalCountries={countries.length} paginate={paginate}/>
-                <button className={"btn"} onClick={nextPage}>NextPage</button>
+                {filteredCountries.map(country => <Countries key={country.name.common} country={country}/>)}
             </div>
         </div>
     );
